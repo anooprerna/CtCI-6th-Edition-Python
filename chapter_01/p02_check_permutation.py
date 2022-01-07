076b03f5527e9f75b8be41bda6d50a25c65df299
anooprerna <44830453+anooprerna@users.noreply.github.com>
2022-01-07 10:02:13 +0530

    Update p02_check_permutation.py

--- chapter_01/p02_check_permutation.py
+++ chapter_01/p02_check_permutation.py
@@ -7,10 +7,11 @@ def check_permutation_by_sort(s1, s2):
     if len(s1) != len(s2):
         return False
     s1, s2 = sorted(s1), sorted(s2)
-    for i in range(len(s1)):
-        if s1[i] != s2[i]:
-            return False
-    return True
+    return s1 == s2
+#     for i in range(len(s1)):
+#         if s1[i] != s2[i]:
+#             return False
+#     return True
 
 
 def check_permutation_by_count(str1, str2):
